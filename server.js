046716002d7e8001b1663f2db19ae42b179cc7bc
J-s-stilo <delightedinternationaldelivery@gmail.com>
2026-08-28 23:51:44 +0100

    Refactor supportId handling in server.js

--- server.js
+++ server.js
@@ -4019,13 +4019,7 @@ app.post('/api/support', auth, writeLimiter, async (req, res) => {
 
       
 
-        supportId,
-
-        [req.user.id,
-
-        message
-
-      ]
+        [supportId, req.user.id, message]
 
     );
 
